5b6eec3c5ddd15d686e83dc38bc3d8af3e12f8da
6gold <15978937635@163.com>
2018-05-07 17:24:42 +0800

    load stl & draw sphere1

--- public/javascripts/3D_S.js
+++ public/javascripts/3D_S.js
@@ -1,10 +1,17 @@
 'use strict';
 
-//
 var object3D_S; // model imported
-var center_3D_S;
 var boundingBox_3D_S;
-var rotation_3D_S;
+var sphere_3D_S; // sphere around the model
+var sphere_pos_3D_S;
+var sphere_radius_3D_S;
+var sphere_wSegs_3D_S;
+var sphere_hSegs_3D_S;
+var sphere_phiStart_3D_S;
+var sphere_phiLen_3D_S;
+var sphere_thetaStart_3D_S;
+var sphere_thetaLen_3D_S;
+
 
 // get canvas
 var canvas_3D_S = document.getElementById('canvas_3D_S');
@@ -51,52 +58,109 @@ controller_3D_S.rotateSpeed = 2.5;
 controller_3D_S.zoomSpeed = 1.0;
 controller_3D_S.panSpeed = 0.6;
 
-// set loader
-var objLoader_3D_S = new THREE.OBJLoader2();
-var callbackOnLoad_3D_S = function (event) {
-    console.log(event.detail);
-    object3D_S = event.detail.loaderRootNode;
-    // scale the model
-    object3D_S.scale.set(5,5,5);
-    // calculate the center point
-    center_3D_S = object3D_S.position;
-    console.log(center_3D_S);
-    // calculate the bounding box
-    boundingBox_3D_S = new THREE.Box3().setFromObject(object3D_S);
+// set OBJ loader
+// var objLoader_3D_S = new THREE.OBJLoader2();
+// var callbackOnLoad_3D_S = function (event) {
+//     console.log(event.detail);
+//     object3D_S = event.detail.loaderRootNode;
+//     // scale the model
+//     object3D_S.scale.set(5,5,5);
+//     // calculate the center point
+//     center_3D_S = object3D_S.position;
+//     console.log(center_3D_S);
+//     // calculate the bounding box
+//     boundingBox_3D_S = new THREE.Box3().setFromObject(object3D_S);
+//     console.log(boundingBox_3D_S);
+//     // calculate the Euler rotation
+//     rotation_3D_S = object3D_S.rotation;
+//     console.log(rotation_3D_S);
+//     // calculate number of vertexes and faces
+//     // console.log(objLoader_3D_S);
+//     console.log(controller_3D_S);
+//
+//     // add model to scene
+//     scene_3D_S.add(object3D_S);
+//
+//     console.log(camera_3DS);
+// };
+// objLoader_3D_S.load('assets/models/obj/Lockheed C130 Hercules.obj', callbackOnLoad_3D_S, null, null, null, false);
+
+// set STL loader
+var stlloader_3D_S=new THREE.STLLoader();
+var callbackOnLoadSTL_3D_S = function (modelgeom) {
+
+    // ---------------------------create model-----------------------------------
+    var modelmat_3D_S = new THREE.MeshBasicMaterial(
+        {
+            transparency: true,
+            opacity: 1,
+            wireframeLinewidth: 0.5,
+            color: 0x444444
+        }
+    );
+    modelmat_3D_S.wireframe = true;
+    object3D_S = new THREE.Mesh(modelgeom, modelmat_3D_S);
+    // calculate boundingBox
+    boundingBox_3D_S = new THREE.Box3().setFromObject(object3D_S);// bounding box
     console.log(boundingBox_3D_S);
-    // calculate the Euler rotation
-    rotation_3D_S = object3D_S.rotation;
-    console.log(rotation_3D_S);
     // calculate number of vertexes and faces
-    // console.log(objLoader_3D_S);
-    console.log(controller_3D_S);
+    // ......
 
-    // add model to scene
+    // object3D_S.position.set( 0, - 0.25, 0.6 );
+    object3D_S.rotation.set( - Math.PI / 2, 0, 0 );
+    object3D_S.scale.set( 20, 20, 20);
+    // add mesh to the scene
     scene_3D_S.add(object3D_S);
+
+    // ------------------------------draw sphere---------------------------------
+    // create radius and position
+    var sqrt_3D_S = Math.sqrt(
+        (boundingBox_3D_S.max.x - boundingBox_3D_S.min.x) * (boundingBox_3D_S.max.x - boundingBox_3D_S.min.x) +
+        (boundingBox_3D_S.max.y - boundingBox_3D_S.min.y) * (boundingBox_3D_S.max.y - boundingBox_3D_S.min.y) +
+        (boundingBox_3D_S.max.z - boundingBox_3D_S.min.z) * (boundingBox_3D_S.max.z - boundingBox_3D_S.min.z)
+    );
+    sphere_radius_3D_S = Math.ceil(sqrt_3D_S) / 2;
+    sphere_pos_3D_S = new THREE.Vector3(
+        (boundingBox_3D_S.max.x + boundingBox_3D_S.min.x) / 2,
+        (boundingBox_3D_S.max.y + boundingBox_3D_S.min.y) / 2,
+        (boundingBox_3D_S.max.z + boundingBox_3D_S.min.z) / 2
+    );
+
+    // create sphere
+    var sphgeom_3D_S = new THREE.SphereGeometry(sphere_radius_3D_S, 100, 100);
+    var sphmat_3D_S = new THREE.MeshBasicMaterial(
+        {
+            transparency: true,
+            opacity: 0.5,
+            wireframeLinewidth: 0.5,
+            color: 0xbbbbbb
+        }
+    );
+    sphmat_3D_S.wireframe = true;
+    sphere_3D_S = new THREE.Mesh(sphgeom_3D_S, sphmat_3D_S);
+    sphere_3D_S.position.set(0, 0, 0);
+    // sphere_3D_S.rotation.set( - Math.PI / 2, 0, 0 );
+    sphere_3D_S.scale.set( 22, 22, 22);
+    // add sphere to the scene
+    scene_3D_S.add(sphere_3D_S);
 };
-objLoader_3D_S.load('assets/models/obj/Lockheed C130 Hercules.obj', callbackOnLoad_3D_S, null, null, null, false);
+stlloader_3D_S.load('assets/models/stl/test.stl',callbackOnLoadSTL_3D_S);
 
-// set fs
-// var fs = require('fs');
-// var data = fs.readFileSync('http://localhost:3000/assets/rcs/model_bi_f02.000ghz_t90_p-180~180_vv.rcs','utf-8');
-// console.log(data);
-
-
-
-
-// call functions
+// -----------------------------call functions---------------------------------------
 render_3D_S();
 
-// definition of functions
-
+// ---------------------------definition of functions--------------------------------
 /* method: render_3D_S
    args: none
    function: render */
 function render_3D_S() {
     requestAnimationFrame(render_3D_S);
     if (!renderer_3D_S.autoClear) renderer_3D_S.clear();
+
     // update values
     // console.log(camera_3DS.rotation.x);
+    // document.getElementById("center_X_3D_S").innerHTML = camera_3DS.position.x;
+
     controller_3D_S.update();
     renderer_3D_S.render(scene_3D_S,camera_3DS);
 }
